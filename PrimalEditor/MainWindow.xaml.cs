6ffc6130846daef5882ea14706f658471c83e773
pkj7323 <pkj7323@naver.com>
2025-02-27 18:05:44 +0900

    초기 커밋.

---
+++ PrimalEditor/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using PrimalEditor.GameProject1;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimalEditor;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+		Loaded += OnMainWindwoLoaded;
+	}
+	private void OnMainWindwoLoaded(object sender, RoutedEventArgs e)
+	{
+		Loaded -= OnMainWindwoLoaded;
+		OpenProjectBrowserDialog();
+	}
+
+	private void OpenProjectBrowserDialog()
+	{
+	   var projectBrowser = new ProjectBrowserDialog();
+	   if(projectBrowser.ShowDialog() == false)
+		{
+			Application.Current.Shutdown();
+        }
+		else
+		{
+            // Load the project
+        }
+    }
+}
